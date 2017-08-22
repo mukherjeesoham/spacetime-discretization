5c42d31a394ce574870b8445fad34c908042135c
Soham Mukherjee <soham2446@gmail.com>
2017-08-22 19:23:12 +0530

    Updated the code for solving the Laplace equation

--- src/laplace_eq_lagrange_multipliers.py
+++ src/laplace_eq_lagrange_multipliers.py
@@ -31,14 +31,14 @@ if(1):
 #------------------------------------------------
 
 # Construct the derivative operator
-I =  np.eye(N+1)	
+I = np.eye(N+1)	
 D = np.kron(I,np.dot(D0, D0)) + np.kron(np.dot(D1, D1), I)
 
 # construct the weight matrix [XXX: Check]
 V = np.outer(util.clencurt(N), util.clencurt(N))
 W = np.diag(np.ravel(V))
 
-# construct the integral + operator
+# construct the main operator
 A = D.dot(W)
 A = A + np.transpose(A)
 
@@ -46,41 +46,42 @@ A = A + np.transpose(A)
 # Construct templates for boundary operators
 #------------------------------------------------
 
-# create template for the projection operators
+# create template for the projection operators. Choose the boundary points on
+# three sides of the grid and flatten the array out.
 loc_BD = np.zeros((N+1,N+1))
-
-# choose the boundary points on three sides of the grid.
 loc_BD[:, 0] = loc_BD[:, -1]= loc_BD[0] = loc_BD[-1] = 1
-BD = np.diag(np.ravel(loc_BD))
+BD = np.ravel(loc_BD)
 
 #------------------------------------------------
-# Impose boundary conditions
+# Impose boundary conditions [using Lagrange multipliers]
 #------------------------------------------------
 
-# imposing the Dirichlet BCs using Lagrange multipliers
+# Pad the matrix to add extra rows and columns
 A  = np.lib.pad(A,  (0, len(BD[BD==1])), 'constant', constant_values=(0))
 
 # construct the vector to be used 
-# for imposing the boudanry condition
-BC = np.where(np.ravel(loc_BD)==1)[0]
+# for imposing the boundary conditions
+BC = np.where(BD==1)[0]
 for _i, _j in enumerate(np.arange((N+1)**2, (N+1)**2 + len(BD[BD==1]))):
 	A[BC[_i]][_j] = A[_j][BC[_i]] = 1
 
-# construct the final matrix
+# assemble the final matrix F
 F  = A
 
 # construct the vector to be used 
 # for imposing the boudanry condition
-b  = np.zeros((N+1)**2.0)
+b  = np.zeros((N+1)**2)
 
 # set the Dirichlet boundary values
 uu = np.zeros((N+1, N+1))
-uu[:, 0]  = 1
-uu[:, -1] = 1
-uu[0]     = 0
+uu[:, 0]  = 0
+uu[:, -1] = 0
 uu[-1]    = 0
+uu[0]     = - np.sin(np.pi*x)	# imposes at (x, y=1)
 
+uu_boudary = np.copy(uu)
 u = np.ravel(uu)
+print u
 u = u[np.where(np.ravel(loc_BD)==1)[0]]
 b = np.append(b, u)
 
@@ -91,7 +92,7 @@ b = np.append(b, u)
 print("Solving a dimension %r linear system..."%np.shape(F)[0])
 if np.linalg.det(F) != 0:	
 	u = np.linalg.solve(F, b)
-	uu = np.reshape(u[:(N+1)**2.0], (N+1, N+1))
+	uu = np.reshape(u[:(N+1)**2], (N+1, N+1))
 	print("Completed solve.")
 else:
 	print "Encountered Singular Matrix. Aborting solver."
@@ -109,26 +110,36 @@ if(1):	# find Eigenvalues (w) and Eigen vectors (v)
 	plt.savefig("../output/laplace_eigen_values.png")
 	plt.close()
 
+def solution(X, Y):
+	S = (1.0/(np.sinh(np.pi)))*(np.sin(np.pi*(X+1))*np.sinh(np.pi*(Y+1)))
+	return S
+
 if(1):	#plot the solution
 	if(1):	#interpolate or not?
 		print("Interpolating grid.")
-		f = interpolate.interp2d(x, t, uu, kind='cubic')
-		xnew = np.linspace(-1, 1, 40)
-		ynew = np.linspace(-1, 1, 40)
-		Z = f(xnew, ynew)
-		X, Y = np.meshgrid(xnew, ynew)
+		f    = interpolate.interp2d(x, t, uu, kind='cubic')
+		xnew = np.linspace(-1, 1, 30)
+		ynew = np.linspace(-1, 1, 30)
+		Z    = f(xnew, ynew)
+		X, Y = np.meshgrid(xnew, ynew)	
+		S    = solution(X, Y)
+		Err  = np.sqrt(np.mean((Z - S)**2.0))
+		print "N = %r \t Error: %r "%(N+1, Err)
 	else:
-		Z = uu
+		Z    = uu
 		X, Y = np.meshgrid(t, x)
+		S    = solution(X, Y)
+		Err  = np.sqrt(np.mean((Z - S)**2.0))
+		print "N = %r \t Error: %r "%(N+1, Err)
 
 	fig = plt.figure()
 	ax = fig.gca(projection='3d')
 	plt.xlabel("x")
-	plt.ylabel("t")
-	ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1)
-
-
-	plt.savefig("../output/laplace_solution.png")
-	plt.close()
-
-
+	plt.ylabel("y")
+	if(0):	# BCs
+		ax.plot_wireframe(xx, tt, uu_boudary, rstride=1, cstride=1)
+	elif(0):	# analytical solution
+		ax.plot_wireframe(X, Y, S, rstride=1, cstride=1, color='m', linewidth=0.7)
+	else:		# computed solution
+		ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color='b', linewidth=0.5)
+	plt.show()

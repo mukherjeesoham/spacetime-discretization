0b0424180cfedd541139129de545fac4379f3bd7
Soham Mukherjee <soham2446@gmail.com>
2017-09-10 22:09:55 -0400

    Test 2D laplace

--- src/laplace/laplace_1D_dirichlet_BCs.py
+++ src/laplace/laplace_1D_dirichlet_BCs.py
@@ -32,18 +32,33 @@ A = np.dot(W, D)
 # construct the vector
 b  = np.zeros(N+1)
 b[0]  = 0.0
-b[-1] = 1.0
+b[-1] = 2.0
 
+# dirichlet
 A[0] = A[-1] = np.zeros(4)
 A[0][0] = 1
 A[-1][-1] = 1
 
-print A
+# neumann
+A[0, :] = D1[0, :]
+
+#------------------------------------------------
+#analyze A
+#------------------------------------------------
+
+print "\nAnalysing the F matrix..."
+
+w, v = np.linalg.eig(A)
+plt.figure(1)
+plt.semilogy(w, 'ro')
+plt.axhline([0])
+plt.show()
 
 #------------------------------------------------
 # solve the system
 #------------------------------------------------
 u = np.linalg.solve(A, b)
-u = u
+plt.figure(2)
 plt.plot(x,u, 'b-o', markersize=2, linewidth=0.6)
+plt.xlabel("x")
 plt.show()
--- src/laplace/laplace_2D_dirichlet_BCs.py
+++ src/laplace/laplace_2D_dirichlet_BCs.py
@@ -12,7 +12,6 @@ from matplotlib import cm
 from scipy import interpolate
 import time
 
-
 #------------------------------------------------
 # Grid
 #------------------------------------------------
@@ -21,12 +20,12 @@ import time
 N = 40
 
 # Construct Chebyshev differentiation matrices.
-D0, t = util.cheb(N) 
-D1, x = util.cheb(N) 
-xx,tt = np.meshgrid(t,x)
+D0, x = util.cheb(N) 
+D1, y = util.cheb(N) 
+xx, yy = np.meshgrid(x,y)
 
 if(1):
-	util.plotgrid(xx, tt)
+	util.plotgrid(xx, yy)
 
 #------------------------------------------------
 # Construct derivate + integral operators
@@ -35,42 +34,49 @@ if(1):
 # Construct the derivative operator
 I = np.eye(N+1)	
 D = np.kron(I,np.dot(D0, D0)) + np.kron(np.dot(D1, D1), I)
+Dx = np.kron(I, D0)
+Dy = np.kron(D1, I)
 
-# construct the weight matrix [XXX: Check]
+# construct the weight matrix
 V = np.outer(util.clencurt(N), util.clencurt(N))
 W = np.diag(np.ravel(V))
 
 # construct the main operator
 A = W.dot(D)
 # A = A + np.transpose(A)
-
 #------------------------------------------------
 # Construct templates for boundary operators
 #------------------------------------------------
 
-# create template for the projection operators. Choose the boundary points on
+# Choose the boundary points on
 # three sides of the grid and flatten the array out.
-loc_BD = np.zeros((N+1,N+1))
-loc_BD[:, 0] = loc_BD[:, -1]= loc_BD[0] = loc_BD[-1] = 1
-BD = np.ravel(loc_BD)
+loc_BC = np.zeros((N+1,N+1))
+loc_BC[0]     = 1	# (x, 0)
+loc_BC[-1]    = 1	# (x, -1)
+loc_BC[:, 0]  = -1	# (0, y)
+loc_BC[:, -1] = -1	#(-1, y)
+
+BC = np.ravel(loc_BC)
 
 #------------------------------------------------
 # Impose boundary conditions [using Lagrange multipliers]
 #------------------------------------------------
 
-def unitvec(A, index):
+def dirichlet(A, index):
 	A[index] = np.zeros(np.shape(A)[0])
 	A[index][index] = 1.0
 	return A
 
-# construct the vector to be used 
-# for imposing the boundary conditions
-BC = np.where(BD==1)[0]
-for _r in BC:
-	unitvec(A, _r)
+def neumann(A, index):
+	A[index] = Dx[index]
+	return A
 
-# assemble the final matrix F
-F  = A
+# reset matrix A
+for _i,_r in enumerate(np.where(BC!=0)[0]):
+	if BC[_r] == 1:		#impose Dirichlet BCs
+		dirichlet(A, _r)
+	elif BC[_r] == -1:	#impose Neumann BCs
+		neumann(A, _r)
 
 # construct the vector to be used 
 # for imposing the boudanry condition
@@ -81,7 +87,7 @@ uu = np.zeros((N+1, N+1))
 uu[:, 0]  = 0.0
 uu[:, -1] = 0.0
 uu[-1]    = 0.0
-uu[0]     = - np.sin(np.pi*x) #-(x**2.0 - 1)
+uu[0]     = -(x**2.0 - 1) #- np.sin(np.pi*x)
 
 uu_boudary = np.copy(uu)
 b = np.ravel(uu)
@@ -90,47 +96,43 @@ b = np.ravel(uu)
 # solve the system
 #------------------------------------------------
 
-print("Solving a dimension %r linear system..."%np.shape(F)[0])
+print("Solving a dimension %r linear system..."%np.shape(A)[0])
 start = time.time()
-u = np.linalg.solve(F, b)
+u = np.linalg.solve(A, b)
 end = time.time()
 print "Time taken by solver: %rs" %(end - start)
 uu = np.reshape(u[:(N+1)**2], (N+1, N+1))
-print("Completed solve.")
-
 
 #------------------------------------------------
 #analysis
 #------------------------------------------------
 if(0):	# find Eigenvalues (w) and Eigen vectors (v)
 	print "\nAnalysing the F matrix..."
-	w, v = np.linalg.eig(F)
+	w, v = np.linalg.eig(A)
 
 	plt.semilogy(w, 'ro')
 	plt.axhline([0])
-	plt.title("Eigen values for a %r-dimensional matrix"%(np.shape(F)[0]))
-	plt.savefig("../output/laplace_eigen_values.png")
+	plt.title("Eigen values for a %r-dimensional matrix"%(np.shape(A)[0]))
+	plt.savefig("../../output/laplace_eigen_values.png")
 	plt.close()
 
-def solution(X, Y):
-	S = (1.0/(np.sinh(2*np.pi)))*(np.sin(np.pi*(X+1))*np.sinh(np.pi*(Y+1)))
-	return S
 
 if(1):	#plot the solution
 	if(0):	#interpolate or not?
 		print("Interpolating grid.")
-		f    = interpolate.interp2d(x, t, uu, kind='cubic')
-		xnew = np.linspace(-1, 1, 30)
-		ynew = np.linspace(-1, 1, 30)
+		S =  np.loadtxt("solution.dat")
+		f    = interpolate.interp2d(x, y, uu, kind='cubic')
+		xnew = np.linspace(-1, 1, np.shape(S)[0])
+		ynew = np.linspace(-1, 1, np.shape(S)[1])
 		Z    = f(xnew, ynew)
 		X, Y = np.meshgrid(xnew, ynew)	
-		S    = solution(X, Y)
-		Err  = np.sqrt(np.mean((Z - S)**2.0))
-		print "N = %r \t Error: %r "%(N+1, Err)
+
+
 	else:
 		Z    = uu
-		X, Y = np.meshgrid(t, x)
-		S    = solution(X, Y)
+		X, Y = np.meshgrid(x, y)
+		S =  np.loadtxt("../solutions/solution_chebnodes.dat")
+
 		Err  = np.sqrt(np.mean((Z - S)**2.0))
 		print "N = %r \t Error: %r "%(N+1, Err)
 
@@ -139,9 +141,10 @@ if(1):	#plot the solution
 	plt.xlabel("x")
 	plt.ylabel("y")
 	if(0):	# BCs
-		ax.plot_wireframe(xx, tt, uu_boudary, rstride=1, cstride=1)
-	elif(0):	# analytical solution
-		ax.plot_wireframe(X, Y, S, rstride=1, cstride=1, color='r', linewidth=0.7)
-	else:		# computed solution
-		ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color='b', linewidth=0.4)
+		ax.plot_wireframe(xx, yy, uu_boudary, rstride=1, cstride=1)
+	elif(1):	# analytical solution
+		ax.plot_wireframe(X, Y, S, rstride=1, cstride=1, color='r', linewidth=0.4)
+	# else:		# computed solution
+		# ax.plot_wireframe(X, Y, Z, rstride=1, cstride=1, color='b', linewidth=0.4, linestyle='--')
+		ax.scatter(X, Y, Z, s=2.4)
 	plt.show()
